1b1cd118355f33d36258d1387c969ff3a77e79ce
napori0624 <naporin0624@gmail.com>
2020-03-28 21:33:57 +0900

    :+1: create module

---
+++ src/client/client.provider.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@nestjs/common";
+import { Configuration } from "webpack";
+import webpack from "webpack";
+import { AbstractHttpAdapter } from "@nestjs/core";
+import webpackDevMiddleware from "webpack-dev-middleware";
+
+@Injectable()
+export class ClientProvider {
+  async register(app: AbstractHttpAdapter, config: Configuration) {
+    const compiler = webpack({
+      ...config,
+      mode: "development",
+      plugins: [
+        ...(config.plugins || []),
+        new webpack.HotModuleReplacementPlugin(),
+      ],
+    });
+
+    app.use(webpackDevMiddleware(compiler));
+  }
+}
